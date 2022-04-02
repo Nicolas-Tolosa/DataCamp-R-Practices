f4f187d0eafbab5d99e8a63bc87b3caaaa11c205
Nicolás Tolosa Riveros <nicolas.tolosar@gmail.com>
2022-04-02 17:06:07 -0300

    Update 2_Whats a factor and why would you use it.R
    Updating some R code style guide

--- 2_Whats a factor and why would you use it.R
+++ 2_Whats a factor and why would you use it.R
@@ -6,7 +6,7 @@ sex_vector <- c("Male", "Female", "Female", "Male", "Male")
 # Convert sex_vector to a factor
 factor_sex_vector <- factor(sex_vector)
 
-# Print out factor_sex_vector
+# Print out
 factor_sex_vector
 
 
@@ -18,7 +18,11 @@ factor_animals_vector
 
 # Temperature
 temperature_vector <- c("High", "Low", "High", "Low", "Medium")
-factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
+factor_temperature_vector <- factor(temperature_vector, 
+                                    order = TRUE, 
+                                    levels = c("Low", "Medium", "High"))
+
+# Print out
 factor_temperature_vector 
 
 
@@ -30,7 +34,7 @@ factor_survey_vector <- factor(survey_vector)
 #Specify the levels of factor_survey_vector
 levels(factor_survey_vector) <- c("Female", "Male")
 
-# Print out factor_survey_vector
+# Print out
 factor_survey_vector
 
 # Generate summary for survey_vector
